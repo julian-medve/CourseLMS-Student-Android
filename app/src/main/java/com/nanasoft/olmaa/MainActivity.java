4cce2aca35d85c4fe0a19dfbd2fac1ade5a9c38f
julian-medve <julian.medve.superdev@gmail.com>
2021-04-30 03:10:30 +0900

    Webview with Vimeo ID

--- app/src/main/java/com/nanasoft/olmaa/MainActivity.java
+++ app/src/main/java/com/nanasoft/olmaa/MainActivity.java
@@ -10,12 +10,15 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.WindowManager;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.androidnetworking.AndroidNetworking;
 import com.androidnetworking.common.Priority;
 import com.androidnetworking.error.ANError;
 import com.androidnetworking.interfaces.JSONObjectRequestListener;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
@@ -34,9 +37,14 @@ public class MainActivity extends AppCompatActivity {
                 new String[]{Manifest.permission.INTERNET},
                 1);
 
+        // Initialize Android Networking
         AndroidNetworking.initialize(getApplicationContext());
 
+
         setContentView(R.layout.activity_main);
+        WebView webview = (WebView) findViewById(R.id.webView);
+        webview.setWebViewClient(new WebViewClient());
+        webview.loadUrl("https://google.com");
 
         // Get params from the URI
         Intent intent = getIntent();
@@ -61,6 +69,21 @@ public class MainActivity extends AppCompatActivity {
                         @Override
                         public void onResponse(JSONObject response) {
                             Log.d(TAG, response.toString());
+
+                            try {
+                                String provider = response.getString("provider");
+                                String url = response.getString("url");
+
+                                String htmlContent = "<body style=\"display: flex;  justify-content: center;  align-items: center;\" >";
+                                if (provider.contains("Vimeo"))
+                                    htmlContent += String.format("<iframe src=\"https://player.vimeo.com/video/%s\" frameborder=\"0\" allow=\"autoplay; fullscreen\" allowfullscreen></iframe>", url);
+
+                                webview.setWebViewClient(new WebViewClient());
+                                webview.loadData(htmlContent, "text/html", "UTF-8");
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
                         }
 
                         @Override
