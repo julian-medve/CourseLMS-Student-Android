90147f89b3275a504eb4412d48f05c9d8449c9f2
julian-medve <julian.medve.superdev@gmail.com>
2021-04-29 23:43:11 +0900

    Deep Link implemented from browser

--- app/src/main/java/com/nanasoft/olmaa/MainActivity.java
+++ app/src/main/java/com/nanasoft/olmaa/MainActivity.java
@@ -2,10 +2,19 @@ package com.nanasoft.olmaa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.WindowManager;
 
+import java.util.Set;
+
+import static android.provider.ContactsContract.CommonDataKinds.Website.URL;
+
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +24,19 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE,
                 WindowManager.LayoutParams.FLAG_SECURE);
 
-
         setContentView(R.layout.activity_main);
+
+        // Get params from the URI
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        Uri data = intent.getData();
+
+        Log.d(TAG, action);
+        if (data != null) {
+            String user = data.getQueryParameter("user");
+            String content = data.getQueryParameter("class");
+            String userId = user;
+            String uri = "http://lms.olmaa.net/api/v1/contentWithVideo?userId=" + user + "&contentId=" + content;
+        }
     }
 }
